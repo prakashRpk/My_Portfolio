8904fd1fa3872a033c6553cf83d89e41b520bbe7
prakashRpk <prakashrprakashr127@gmail.com>
2024-10-09 09:34:34 +0530

    submit files

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var circle=document.getElementById("Index-circle")
 var navbar=document.querySelector(".navbar")
 var nav_item=document.querySelector(".index-ul")
+var video = document.getElementById("video")
 var times=true
 function hide(){
     if(times==true){
@@ -19,3 +20,32 @@ else{
     times=true
 }
 }
+function CloseNav(){
+    circle.style.left="95%"
+    navbar.style.left="95%"
+    navbar.style.transition="0.5s"
+    circle.style.transition="0.5s"
+    nav_item.style.transition="0.5s"
+    nav_item.style.left="110%"
+}
+var msg=document.getElementById("massage")
+function BtnMsg(){
+    msg.style.marginLeft="70%"
+    msg.style.transition="0.5s"
+    setTimeout(back,5000)
+}
+function back(){
+    msg.style.marginLeft="100%"
+}
+function videoFun(){
+    if(times==true){
+     video.style.right="850px"
+    video.style.transition="0.5s"
+    times=false
+}
+else{
+    video.style.right="2000px"
+    times=true
+}
+}
+
--- js/scroll bar.js
+++ js/scroll bar.js
@@ -11,9 +11,3 @@ document.getElementById("con-left").onclick = function() {
   document.getElementById("con-right").onclick = function() {
     document.getElementById("scroll-con").scrollBy(250, 0);
   }
-  document.getElementById("con-left-pro").onclick = function() {
-    document.getElementById("scroll-con-pro").scrollBy(-960, 0);
-  }
-  document.getElementById("con-right-pro").onclick = function() {
-    document.getElementById("scroll-con-pro").scrollBy(960, 0);
-  }
